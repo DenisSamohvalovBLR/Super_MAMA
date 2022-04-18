dac3100f9a23ce94c5d97371b3fde158e5d4cc54
DenisSamohvaBLR <Denis.Samohvalov.1987.BLR@gmail.com>
2022-04-18 15:07:46 +0300

    added parser 2

--- src/controller/parse.py
+++ src/controller/parse.py
@@ -1,17 +1,19 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+ua = UserAgent()
 
 
 def get_data():
-    headers = {
-        'user-agent': 'Mozilla / 5.0(X11; Linux x86_64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 97.0 .4692 .71 Safari / 537.36'
-    }
-
-    r = requests.get(url='https://by.detmir.com/catalog/index/name/sortforsale/is_sale/1/categories-44050',
-                     headers=headers)
+    response = requests.get(
+        url='https://by.detmir.com/catalog/index/name/sortforsale/is_sale/1/categories-44050',
+        headers={'user-agent': f'{ua.random}'})
 
     with open('data/index.html', 'w') as file:
-        file.write(r.text)
+        file.write(response.text)
 
     with open('data/index.html') as file:
         src = file.read()
@@ -31,6 +33,13 @@ def get_data():
 
     for el in result:
         print(el)
+
+    with open('result.json', 'w') as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
+
+    with open('data/result.json') as file:
+        all_categories = json.load(file)
+    print(all_categories)
 
 
 def main():
